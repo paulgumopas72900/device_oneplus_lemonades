c49491aa791e31b41d1c5b728ff75a8192b14c51
AnierinB <anierin@evolution-x.org>
2023-04-28 19:43:28 +0530

    lemonades: Configure OPlusExtras device specific features
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -29,6 +29,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
+    AospSystemUIResTarget \
+    OPlusExtrasResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
